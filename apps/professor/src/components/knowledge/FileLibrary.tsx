59ceaf40038bf707ed5b5ef49fbbe9df2519b8da
Yung Kim <yungkim@tecace.com>
2025-10-16 17:27:13 -0700

    Apply inline width styles to File Library table headers
    Changes:
    - Added inline width styles directly to <th> elements
    - Content Type column now explicitly set to 12% width (was taking more space)
    - Column width distribution:
      * File Name: 30%
      * Size: 10%
      * Last Modified: 15%
      * Content Type: 12% (reduced!)
      * Sync Status: 13%
      * Actions: 20%
    - All columns have min-width for responsiveness
    - Inline styles ensure immediate application without cache issues

--- apps/professor/src/components/knowledge/FileLibrary.tsx
+++ apps/professor/src/components/knowledge/FileLibrary.tsx
@@ -548,12 +548,12 @@ const FileLibrary: React.FC = () => {
           <table className="fl-table">
             <thead>
               <tr>
-                <th>{t('knowledge.fileName')}</th>
-                <th>{t('knowledge.size')}</th>
-                <th>{t('knowledge.lastModified')}</th>
-                <th>{t('knowledge.contentType')}</th>
-                <th style={{ textAlign: 'center' }}>{t('knowledge.syncStatus')}</th>
-                <th>{t('knowledge.actions')}</th>
+                <th style={{ width: '30%', minWidth: '200px' }}>{t('knowledge.fileName')}</th>
+                <th style={{ width: '10%', minWidth: '80px' }}>{t('knowledge.size')}</th>
+                <th style={{ width: '15%', minWidth: '130px' }}>{t('knowledge.lastModified')}</th>
+                <th style={{ width: '12%', minWidth: '100px' }}>{t('knowledge.contentType')}</th>
+                <th style={{ width: '13%', minWidth: '110px', textAlign: 'center' }}>{t('knowledge.syncStatus')}</th>
+                <th style={{ width: '20%', minWidth: '140px' }}>{t('knowledge.actions')}</th>
               </tr>
             </thead>
             <tbody>
