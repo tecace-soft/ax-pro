20150f3b69732a4ed4fc38f4ea5b75f53dfed4c0
unknown <genekim@tecace.com>
2025-12-19 17:29:00 -0800

    Added new custom domain

--- apps/professor/vite.config.ts
+++ apps/professor/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     port: 3000,
     open: true,
     host: '0.0.0.0',
-    allowedHosts: ['professor-ax-pro.onrender.com', 'localhost', '127.0.0.1']
+    allowedHosts: ['professor-ax-pro.onrender.com', 'ax-pro.tecace.com', 'localhost', '127.0.0.1']
   }
 })
